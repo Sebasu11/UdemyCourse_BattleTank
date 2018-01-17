1dd8c464c7cecd1ceeab9df9422bb9b618d09443
DESKTOP-PD8L9VN\User <sebasuribe07@gmail.com>
2018-01-17 16:53:24 -0500

    BlueprintCallable - Working

--- BattleTank/Source/BattleTank/Public/Tank.h
+++ BattleTank/Source/BattleTank/Public/Tank.h
@@ -14,6 +14,9 @@ public:
 
 	void AimtAt(FVector HitLocation);
 
+	UFUNCTION(BlueprintCallable, Category=Setup)
+	void SetBarrelReference(UStaticMeshComponent* BarrelToSet);
+
 protected:
 	UTankAimingComponent* TankAimingComponent = nullptr;
 
