5718cbde20919a185977abf1e82b3acbdb7598a1
DESKTOP-PD8L9VN\User <sebasuribe07@gmail.com>
2018-01-18 11:15:20 -0500

    Predict Projectile Landing Point - working

--- BattleTank/Source/BattleTank/Private/TankAimingComponent.cpp
+++ BattleTank/Source/BattleTank/Private/TankAimingComponent.cpp
@@ -40,12 +40,27 @@ void UTankAimingComponent::TickComponent( float DeltaTime, ELevelTick TickType, 
 
 void UTankAimingComponent::AimAt(FVector HitLocation, float LaunchSpeed)
 {
-	auto OurTankName = GetOwner()->GetName();
-	auto BarrelLocation = Barrel->GetComponentLocation().ToString();
+	//auto OurTankName = GetOwner()->GetName();
+	//auto BarrelLocation = Barrel->GetComponentLocation().ToString();
 	// Checks tank name, where is it pointing where is the barrel located
 	//UE_LOG(LogTemp, Warning, TEXT("%s aiming at %s from %s"), *OurTankName, *HitLocation.ToString(),*BarrelLocation);
-	UE_LOG(LogTemp, Warning, TEXT(" Firing at %f "), LaunchSpeed);
+	//UE_LOG(LogTemp, Warning, TEXT(" Firing at %f "), LaunchSpeed);
 
+	if (!Barrel) { return; }
 
-}
+	//const UObject * WorldContextObject;
+	FVector OutLaunchVelocity;
+	FVector StartLocation = Barrel->GetSocketLocation(FName("Projectile"));
+	//FVector EndLocation;
+
+	// Calculation outLaunchVelocity
+	
+		if (UGameplayStatics::SuggestProjectileVelocity(this, OutLaunchVelocity, StartLocation, HitLocation, LaunchSpeed, false, 0, 0, ESuggestProjVelocityTraceOption::DoNotTrace)
+			)
+		{
+			auto AimDirection = OutLaunchVelocity.GetSafeNormal();
+			UE_LOG(LogTemp, Warning, TEXT(" Aiming at %s "), *AimDirection.ToString());
+		}
+	//If no solution found do nothing 
+		}
 
