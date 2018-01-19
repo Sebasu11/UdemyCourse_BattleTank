8508370d7cbbfb5c82306b4740436b58e6c2b506
DESKTOP-PD8L9VN\User <sebasuribe07@gmail.com>
2018-01-18 22:32:15 -0500

    i think this works!

--- BattleTank/Source/BattleTank/Public/TankAimingComponent.h
+++ BattleTank/Source/BattleTank/Public/TankAimingComponent.h
@@ -5,11 +5,9 @@
 #include "Components/ActorComponent.h"
 #include "TankAimingComponent.generated.h"
 
-<<<<<<< HEAD
+
 class UTankBarrel;//Forward declaration -> Allows to reference the utank and use the dependencies
 class UTankTurret;//Forward declaration -> Allows to reference the utank and use the dependencies
-=======
->>>>>>> 08bd16f0103c98e3845f17fa6bd5aaccae39ba99
 
 UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
 class BATTLETANK_API UTankAimingComponent : public UActorComponent
@@ -20,19 +18,15 @@ public:
 	// Sets default values for this component's properties
 	UTankAimingComponent();
 
-<<<<<<< HEAD
 	void SetBarrelReference(UTankBarrel* BarrelToSet);
 	void SetTurretReference(UTankTurret* TurretToSet);
 
-=======
->>>>>>> 08bd16f0103c98e3845f17fa6bd5aaccae39ba99
 	// Called when the game starts
 	virtual void BeginPlay() override;
 	
 	// Called every frame
 	virtual void TickComponent( float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction ) override;
 
-<<<<<<< HEAD
 	void AimAt(FVector HitLocation, float LaunchSpeed);
 
 	
@@ -43,10 +37,6 @@ private:
 	UTankTurret* Turret = nullptr;
 
 	void MoveBarrelTowards(FVector AimDirection);
-=======
 	void AimAt(FVector HitLocation);
-
-		
->>>>>>> 08bd16f0103c98e3845f17fa6bd5aaccae39ba99
 	
 };
